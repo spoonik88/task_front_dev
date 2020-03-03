37ae9711d048b66166cfea671f8674d16d9ab626
fulexdevelopment <fulexdevelopment@yandex.ru>
2020-03-03 03:48:24 +0300

    option for country and YMaps.location

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
+const
+    countryEl = document.querySelector('#nationality'),
+    country_user = YMaps.location.country;
 
+let country = [
+    { id: '0', country: country_user },
+    { id: '1', country: 'Россия' },
+    { id: '2', country: 'Беларусь' },
+    { id: '3', country: 'Украина' },
+    { id: '4', country: 'America' }
 
-
-  
-});
+];
+countryEl.innerHTML = country.map(n => `<option value="${n.id}" class="form__line-option">${n.country}</option>`).join('');
